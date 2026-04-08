544fd2bcc57f6a6a38baba9a50a367c4c1004569
mozart953 <wilsonnianopop@gmail.com>
2026-04-07 18:10:11 -0600

    feat: HomePage first init

---
+++ corpus-native-languages/components/Home.jsx
@@ -0,0 +1,13 @@
+function HomeComponent(){
+
+    return(
+        <>
+            <div className="FirstCard">
+                <h1>Native Languages Guatemala</h1>
+            </div>
+        </>
+    )
+
+}
+
+export default HomeComponent;
